9af3042b85f77dd630e29047030e001305748a24
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-09 10:03:53 -0700

    added await to async call

--- sources/editor/Stride.Assets.Presentation/ViewModel/ScriptSourceFileAssetViewModel.cs
+++ sources/editor/Stride.Assets.Presentation/ViewModel/ScriptSourceFileAssetViewModel.cs
@@ -292,7 +292,7 @@ namespace Stride.Assets.Presentation.ViewModel
                         if (diskContent != Asset.Text)
                         {
                             // Prompt for reload from source file
-                            Dispatcher.Invoke(() => UpdateAssetFromSource(new LoggerResult()));
+                            await Dispatcher.Invoke(() => UpdateAssetFromSource(new LoggerResult()));
                         }
                     }
                     catch (Exception)
